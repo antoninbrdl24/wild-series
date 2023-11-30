bac8e00477d6d6f5c3cb957e7d5bfb3739e4170a
antoninbrdl24 <antonin.brondel24@gmail.com>
2023-11-30 10:59:51 +0100

    fin de la quête 10

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,28 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProgramRepository;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(ProgramRepository $programRepository): Response
     {
-        return $this->render('program/index.html.twig', [
-           'website' => 'Wild Series',
-        ]);
-    }
-    #[Route('/program/{id}/', name: 'program_show', requirements: ['id' => '\d+'], methods: ['GET'])]
-    public function show(int $id): Response
+        $programs = $programRepository->findAll();
+    
+        return $this->render(
+            'program/index.html.twig',
+            ['programs' => $programs]
+            );
+        }
+    
+    #[Route('/show/{id<^[0-9]+$>}', name: 'show')]
+    public function show(int $id, ProgramRepository $programRepository):Response
     {
-        // Load program data from the database based on $id
-        // Replace this with your actual logic to fetch program data
-        $programData = [
-            'id' => $id,
-            // Add other program data fields here
-        ];
-
+        $program = $programRepository->findOneBy(['id' => $id]);
+        // same as $program = $programRepository->find($id);
+        
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
         return $this->render('program/show.html.twig', [
-            'id' => $id,
+            'program' => $program,
         ]);
     }
 }
